c2febcd90164bbc3a5b1c547bdb7a8d4e0a154ea
Josh Smith <joshsmith2@gmail.com>
2015-03-26 18:08:04 +0000

    Added self.current_dir to base.py

--- tests/base.py
+++ tests/base.py
@@ -1,9 +1,11 @@
 import unittest
+import os
 
 class GeneralTest(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.current_dir = os.path.dirname(os.path.abspath(__file__))
+
 
     def tearDown(self):
         pass
