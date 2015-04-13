26963731b26b58216ab9ca58c1da9d702fb2b876
Josh Smith <joshsmith2@gmail.com>
2015-04-13 21:35:54 +0100

    New functional test to work on - load data from csv

--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -27,6 +27,32 @@ class FunctionalTest(GeneralTest):
         for result in [green_title, green_user, '08/04/2015']:
             self.assertIn(result, results)
 
+    def test_can_get_output_from_csv_file(self):
+        # We have a single rowed file full of URLs, and we feed it to our
+        # script.
+        csv_in = os.path.join(self.current_dir, 'files', 'test_csv_in.csv')
+        sp.check_call([self.script,
+                       '-c', csv_in,
+                       '-o', self.output])
+
+        # There's a file output...
+        self.assertTrue(os.path.exists(self.output))
+
+        # ...with 3 results in it (and a header)
+        with open(self.output, 'r') as f:
+            contents = f.readlines()
+        self.assertEqual(len(contents), 4)
+        headers = contents[0].split(',')
+        results = [c.split(',') for c in contents [1:]]
+        for header in ['name', 'view_count', 'comment_count']:
+            self.assertIn(header, headers)
+
+        # Further, these are the videos we'd expected to see
+        names = ['George Galloway MP on Chilcot Inquiry Delays',
+                 'Parliamentary Debate on Epilepsy - 26th February',
+                 "Forced Marriage - It's Never OK."]
+        for name in names:
+            self.assertIn(name, results)
 
 if __name__ == '__main__':
      unittest.main()
